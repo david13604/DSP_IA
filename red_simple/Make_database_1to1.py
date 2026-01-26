ce85a80e0feb69a5acd9f82fd7e23d720a3c51f9
david13604 <darodriguez8@uc.cl>
2026-01-25 22:37:07 -0300

    cambios menores

--- red_simple/Make_database_1to1.py
+++ red_simple/Make_database_1to1.py
@@ -7,8 +7,9 @@ import matplotlib.pyplot as plt
 
 def process_audio_file(path):
     # Takes fft
-    y = librosa.load(path, sr=44100, mono=True)
+    y, sr = librosa.load(path, sr=44100, mono=True)
     fft = tf.signal.fft(y)
+    print(len(fft))
     return fft
 
 
@@ -16,13 +17,13 @@ def process_folder(root_folder, save_path="dataset.npz"):
     X = []
     for root, _, files in os.walk(root_folder):
         for file in files:
-            if file.lower().endswith(".wav"):
+            if file.lower().endswith(".mp3"):
                 full_path = os.path.join(root, file)
                 try:
                     # Guardamos fft
                     print(f"Processing: {full_path}")
                     segments = process_audio_file(full_path)
-                    X.extend(segments)
+                    X.append(segments)
                 except Exception as e:
                     print(f"Failed to process {full_path}: {e}")
     np.savez_compressed(save_path, X=tf.convert_to_tensor(X))
@@ -30,6 +31,6 @@ def process_folder(root_folder, save_path="dataset.npz"):
 
 
 if __name__ == "__main__":
-    root_single = "/mnt/c/Users/matth/OneDrive/Desktop/PUC/DSP_IA/red_simple/db"
+    root_single = r"C:\Users\usuario\Desktop\DSP_IA_local\red_simple" #cambiele o le tira error al que lea
+    print("\n ************************ \n")
     process_folder(root_single, save_path="dataset_single.npz")
-
